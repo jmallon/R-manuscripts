8acb978d555f15e5e3b073645a69340c9c01a89a
jmallon <jmmallon@umd.edu>
2018-01-10 14:04:28 -0500

    updated file_dir_params
    made paths generic for any user, added annotations

--- proposal/chapter 1/functions/file_dir_params.R
+++ proposal/chapter 1/functions/file_dir_params.R
@@ -1,6 +1,8 @@
 #--begin file_dir_params.R script--#
-#from  http://derekyves.github.io/2016/05/10/codeshare.html
+### Credit goes to Derek Darves
+### Step by step explanations availble at http://derekyves.github.io/2016/05/10/codeshare.html
 
+#----------------------------#
 # Make a new environment:
 fdirs <- new.env()
 
@@ -23,24 +25,23 @@ get_os <- function(){
 fdirs$computeros <- get_os()
 
 ## Declare the root project and data directory:
-
 show  <- 1
-#build <- 1
+#build <- 1                                                   #use if you are creating the directories for the first time
 
 if(grepl("windows", fdirs$computeros)==F){
-  fdirs$prjdir <- "~/Box Sync/R/Chapter 1/Scripts/"
-  fdirs$prjdta <- "./Data/"
-}else{
-  fdirs$prjdir <- "C:/Users/Julie/Box Sync/R/Chapter 1/Scripts/"
-  fdirs$prjdta <- "./Data/" #update for windows 
+  fdirs$prjdir <- "~/mypath/R-manuscripts/Chapter 1/scripts/"
+  fdirs$prjdta <- "./data/"
+}else{                                                        #using windows machine
+  fdirs$prjdir <- "C:/mypath/R-manuscripts/Chapter 1/scripts/"
+  fdirs$prjdta <- "./data/" 
 }
 
 # Add some child objects to the fdirs environment:
-fdirs$prjrslts   <- paste0(fdirs$prjdta, "Results/")
+fdirs$prjrslts   <- paste0(fdirs$prjdta, "results/")
 if(show==1 & interactive()) cat("\nprjrslts =", fdirs$prjrslts) #?
 #if(build==1) system(paste0("mkdir -p ", fdirs$prjrslts)) #?
 
-fdirs$prjfuns   <- paste0(fdirs$prjdir, "Functions/")
+fdirs$prjfuns   <- paste0(fdirs$prjdir, "functions/")
 if(show==1 & interactive()) cat("\nprjfuns =", fdirs$prjfuns) #?
 #if(build==1) system(paste0("mkdir -p ", fdirs$prjfuns)) #?
 
